8df0dea4bc1c68d2b3103b816c98955e04b62787
Alwell <robinalwell@Robins-MacBook-Pro.local>
2015-05-05 22:31:55 -0400

    test

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+alex = User.create(username: 'Alex Perla', avatar:'/images/alex.png', subscribers: 1423459 )
+mike = User.create(username: 'Mike Perla', avatar:'/images/mike.png', subscribers: 1459934)
