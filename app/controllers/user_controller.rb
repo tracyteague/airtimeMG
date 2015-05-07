4fbc19a2d566b1b02a1b076ba69a51a6497071b4
Alwell <robinalwell@Robins-MBP.wp.comcast.net>
2015-05-07 15:16:57 -0400

    test

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,21 @@
 class UserController < ApplicationController
 	def index
-		@user = User.create(params)
+
 	end
 
 	def create
-		@user = User.create(params)
+	  @user = User.new(user_params)
+	  if @user.save
+	    session[:user_id] = @user.id
+	    redirect_to '/'
+	  else
+	    redirect to '/'
+	  end
+	end
+
+	private
+
+	def user_params
+	  params.require(:user).permit(:username, :avatar, :subscribers)
 	end
 end
